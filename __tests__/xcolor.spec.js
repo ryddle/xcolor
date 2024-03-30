12611dc02abc691a89ae1501304b84a73b59de16
admin <izar.nekkar@gmail.com>
2024-03-30 15:57:05 +0100

    translated texts

--- __tests__/xcolor.spec.js
+++ __tests__/xcolor.spec.js
@@ -2,64 +2,65 @@ const xcolor = require('../xcolor');
 const { expect, describe, it } = require('@jest/globals');
 
 describe('xcolor', () => {
-  it('crea instancias', () => {
+  it('creates instances', () => {
     //const c = new xcolor('red');
     //expect(c).not.toBeInstanceOf(xcolor);
     expect(new xcolor('red')).toThrowErrorMatchingSnapshot();
   });
 
-  it('muestra el color en hex', () => {
+  it('shows color in hex', () => {
     const c = new xcolor('#ff0000');
     expect(c.getHex()).toBe('#ff0000');
   });
 
-  it('comprueba igualdad de colores', () => {
+  it('checks color equality', () => {
     const c1 = new xcolor('red');
     const c2 = new xcolor('#ff0000');
     expect(c1).not.toBe(c2);
   });
 
-  it('comprueba diferencia de colores', () => {
+  it('checks color difference', () => {
     const c1 = new xcolor('red');
     const c2 = new xcolor('blue');
     expect(c1).not.toBe(c2);
   });
 
-  it('muestra el color en rgba', () => {
+  it('shows color in rgba', () => {
     const c = new xcolor('rgba(255,0,0,0.5)');
     expect(c.getRgba()).toBe('rgba(255,0,0,0.5)');
   });
 
-  it('muestra el color en rgb', () => {
+  it('shows color in rgb', () => {
     const c = new xcolor('rgba(255,0,0,0.5)');
     expect(c.getRgb()).toBe('rgb(255,0,0)');
   });
 
 
-  it('genera un color hexadecimal aleatorio', () => {
+  it('generates a random hexadecimal color', () => {
     const color = xcolor.randomColor('hex');
     expect(color).toMatch(/^#[0-9a-fA-F]{6}$/);
   });
 
-  it('genera un color rgb aleatorio', () => {
+  it('generates a random rgb color', () => {
     const color = xcolor.randomColor('rgb');
     expect(color).toMatch(/^rgb\(\d+,\d+,\d+\)$/);
   });
 
-  it('genera un color rgba aleatorio', () => {
+  it('generates a random rgba color', () => {
     const color = xcolor.randomColor('rgba');
     expect(color).toMatch(/^rgba\(\d+,\d+,\d+,[\d.]+\)$/);
   });
 
-  it('genera un color hsl aleatorio', () => {
+  it('generates a random hsl color', () => {
     const color = xcolor.randomColor('hsl');
     expect(color).toMatch(/^hsl\(\d+,\d+%,\d+%\)$/);
   });
 
-  it('genera un color hsla aleatorio', () => {
+  it('generates a random hsla color', () => {
     const color = xcolor.randomColor('hsla');
     expect(color).toMatch(/^hsla\(\d+,\d+%,\d+%,[\d.]+\)$/);
   });
 
 
 });
+
